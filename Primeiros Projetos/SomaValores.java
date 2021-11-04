a9f218ddf21aeaec4dd2df17b3a5ed3a5b68f840
BSS2005 <93331078+BSS2005@users.noreply.github.com>
2021-11-04 15:56:55 -0300

    Add files via upload

---
+++ Primeiros Projetos/SomaValores.java
@@ -0,0 +1,15 @@
+
+public class SomaValores {
+      public  static void main(String args[]) {
+    	  int n1, n2, soma;
+    	  
+    	  n1 = 545;
+    	  n2 = 78;
+    	  
+    	  soma = n1+n2;
+    	  
+    	  System.out.println("A soma de "+n1+" + "+n2+" = "+soma);
+    	  
+    	  
+}
+}
