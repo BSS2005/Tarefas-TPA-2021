5a843911579046ef5f341fdfb95158158e1bc2cc
BSS2005 <93331078+BSS2005@users.noreply.github.com>
2021-11-04 15:50:06 -0300

    Create  AloMundo.java

---
+++ Primeiros Projetos/AloMundo.java
@@ -0,0 +1,14 @@
+
+public class AloMundo {  /** o nome do programa*/
+	
+	public static void main (String args[]) { // assinatura do m�todo main
+		System.out.println("Alo Mundo"); 
+		System.out.println("\nHello Word\n");
+	}
+	   
+	    
+	    
+	    
+}
+	
+
