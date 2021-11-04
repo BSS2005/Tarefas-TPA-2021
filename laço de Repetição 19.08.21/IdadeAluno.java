e2517555aef200f178bde708c6b3a69132d60831
BSS2005 <93331078+BSS2005@users.noreply.github.com>
2021-11-04 16:06:49 -0300

    Add files via upload

---
+++ laço de Repetição 19.08.21/IdadeAluno.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class IdadeAluno {
+    public static void main (String args[]) {
+    	Scanner in = new Scanner (System.in);
+    	int i=1;
+    	int idadeAluno, maior=0, menor=0;
+    	   while (i<=10);
+               System.out.println("Entre com a idade do "+i+" aluno:");
+               idadeAluno = in.nextInt();
+    	       idadeAluno++;
+    	       if (idadeAluno<18) {
+    	       System.out.println("Essa(e) aluna(o) é menor de idade.");
+    	       menor++;
+    	       }else {
+    	       System.out.println("Essa(e) aluna(o) é maior de idade.");
+    	       maior++;   
+    	       }
+    	       i++;
+    	       System.out.println("Quantidade de menores"+menor);
+    	       System.out.println("Quantidade de maiores"+maior);
+    }
+}
+    
+    
+
+       
+
